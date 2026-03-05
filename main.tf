1a88354a01d2471abb9f890626eb22c8cf3efdb1
shashi0396 <shashikirandc1315@gmail.com>
2026-03-05 15:26:23 +0530

    adding initial main.tf for infracost estimation

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0f58b397bc5c1f2e8"
+  instance_type = "t2.medium"
+
+  tags = {
+    Name = "WebServer"
+  }
+}
+
+resource "aws_ebs_volume" "data" {
+  availability_zone = "ap-south-1a"
+  size              = "20"
+}
